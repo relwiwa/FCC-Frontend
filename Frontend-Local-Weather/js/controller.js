023a321f2f9ce1e300665322fb93c08bc7c4c24a
Walter <relw1wa-github@yahoo.de>
2016-05-14 15:27:36 +0200

    Reorganize api calls by putting requests from CurrentConditionsCtrl into forecastSvc

--- Frontend-Local-Weather/js/controller.js
+++ Frontend-Local-Weather/js/controller.js
@@ -1,14 +1,15 @@
 /********************************************************
 	CURRENTCONDITIONS CONTROLLER
 	-------------------------------------------------------
-	Responsible for getting local weather conditions
-	- uses to functions for automatic and manual lookup
-		of location, before fetching current conditions
+	responsible for managing local weather conditions
 	- tries to automatically get user's location via
 		geolocation
-	- if geolocation is not available, prompts user to
+	- if geolocation is not available or does not work,
+		uses autoip feature of wunderground api
+	- if autoip does not work either, prompts user to
 		enter location manually
 	- manual search is possible all the time
+	- queries to wunderground api are placed in forecastSvc
 	- in the view, messages are displayed depending on
 		various data being present or set to true/false
 	- controller as-syntax is used as well as vm = this
@@ -22,129 +23,106 @@ angular.module("localWeather").controller("CurrentConditionsCtrl", ["locationSvc
 	// current conditions data is stored here
 	vm.current = {
 		position: {},
-		conditions: {},
-		tempScale: "C"
+		conditions: {}
 	};
 	
-	vm.geoLocSupport = true;
+	vm.automaticSupport = true;
+	vm.tempScale = "C"
 	
 	// data for handling manual search
 	vm.manual = {
 		userInput: "",
-		apiOutput: "",
 		notFound: false,
 		ongoingQuery: false
 	}
 	
-	/* automaticQuery Function
-		- tries to get user's location via geolocation using locationSvc
-		- if user's location is retrieved, gets current weather conditions
-			via forecastSvc.getCurrentConditions and puts results into
-			vm.current.xxx, thus updating view
+	/* automaticQuery function
+		- tries to get user's location via geolocation or autoip feature of
+			weather underground api; geolocation is more accurate, so we try to
+			get user's location with this one first
+		- if user's geolocation is retrieved, gets current weather conditions
+			via forecastSvc.getCurrentConditions and overwriting current object, thus updating view; if request for weather data based on geolocation isn't
+			successful, autoip feature is used
 		- if geolocation is not supported, autoip feature of weather
 			underground api is used instead
-		- if neither geolocation nor autoip workout, vm.geoLocSupport is 		set to false, prompting user in view to manually enter location */
+		- query with autoip feature is put into vm.autoIpQuery function
+		- if neither geolocation nor autoip workout, vm.geoLocSupport is
+			set to false, prompting user in view to manually enter location */
 	vm.automaticQuery = function() {
 		locationSvc.geoLocation.getPosition().then(
-			// use position from geolocation
+			// grabbing location via geolocation was successful
 			function(result) {
 				vm.current.position.coords = result.coords;
-				forecastSvc.getCurrentConditions(vm.current.position).then(
+				forecastSvc.getCurrentConditions().viaCoords(vm.current.position).then(
+					// request for weather data was successful
 					function(result) {
-						vm.current.conditions = result;
-						vm.current.place = vm.current.conditions.data.current_observation.display_location.full;
-						vm.current.temperatureC = vm.current.conditions.data.current_observation.temp_c;
-						vm.current.temperatureF = vm.current.conditions.data.current_observation.temp_f;
-						vm.current.weather = vm.current.conditions.data.current_observation.weather;
-						vm.current.icon = vm.current.conditions.data.current_observation.icon_url;
+						vm.current = result;
+					},
+					// request for weather data wasn't successful, now try autoip feature
+					function() {
+						vm.autoIpQuery();
 					}
 				)
 			},
-			// use autoip feature of weather underground api
-			function(error) {
-				forecastSvc.getCurrentConditions("autoip").then(
-					function(result) {
-						vm.current.conditions = result;
-						vm.current.place = vm.current.conditions.data.current_observation.display_location.full;
-						vm.current.temperatureC = vm.current.conditions.data.current_observation.temp_c;
-						vm.current.temperatureF = vm.current.conditions.data.current_observation.temp_f;
-						vm.current.weather = vm.current.conditions.data.current_observation.weather;
-						vm.current.icon = vm.current.conditions.data.current_observation.icon_url;
-					},
-					// prompt user to manually enter location
-					function(error) {
-						vm.geoLocSupport = false;
-					}
-				)
+			// grabbing location via geolocation was not successful, so use autoip feature of weather underground api
+			function() {
+				vm.autoIpQuery();
 			}
 		); 
 	};
 	
+	/* autoIpQuery function:
+		- uses weather underground api's autoip feature
+		- gets called either when geolocation is disabled or request for
+			weather data based on geolocation wasn't successful
+		-	if autoip request for weather data is also not successful,
+			vm.automaticSupport gets set to false;
+	*/
+	vm.autoIpQuery = function() {
+		forecastSvc.getCurrentConditions().viaAutoIp().then(
+			function(result) {
+				vm.current = result;
+			},
+			// request for weather data based on autoip feature wasn't succesful, so prompt user to manually enter location
+			function() {
+				vm.automaticSupport = false;
+			}
+		);
+	}
+	
 	/* manualQuery Function
 		- enable user to manually enter a location and tries to fetch
 			conditions for the location entered
-		- depends on presence of certain properties in result object
-			for determing success, partial success or failure of request
-			for weather data
-		- if data.current_observation is available, location entered is
-			unique and weather conditions can be put to vm.current.xxx
-		- if data.current_observation doesn't exist, but data.response.results
-			is there, location entered results in multiple resulting locations;
-			strategic decision to use unique zmw-id of first matching location
-			for another request. if user wanted other location, needs to enter
-			the location with more details
-		- if data.current_observation and data.response.results doesn't exist,
-			there is no data for the location entered, vm.manual.notFound gets
-			set to true, informing user in view about no results for this query
-		- vm.manual.userInput stores the location entered by user
-		-	vm.manual.apiOutput stores unique id of first location returned by
-			api in case of no unique location
-		- vm.ongoingQuery is necessary for managing the messages displayed
-			in view	*/
+		- serveral properties are used to correctly display messages in view:
+			--- vm.manual.ongoingQuery is true whenever a manual search is happening
+			--- vm.manual.notFound is true when weather data for location was not found */
 	vm.manualQuery = function() {
 		// set/reset data for proper display of messages in view
 		vm.manual.notFound = false;
 		vm.manual.ongoingQuery = true;
-		vm.current = {
-			position: {},
-			conditions: {},
-			tempScale: vm.current.tempScale
-		};
-		// first run uses userInput, second run uses apiOutput
-		var query = vm.manual.userInput;
-		if (vm.manual.apiOutput !== "") {
-			query = vm.manual.apiOutput;
-		}
-		forecastSvc.getCurrentConditions(query).then(function(result) {
-			// location entered matches unique location
-			if (result.data.current_observation) {
-				vm.manual.ongoingQuery = false;
-				vm.current.conditions = result;
-				vm.current.place = vm.current.conditions.data.current_observation.display_location.full;
-				vm.current.temperatureC = vm.current.conditions.data.current_observation.temp_c;
-				vm.current.temperatureF = vm.current.conditions.data.current_observation.temp_f;
-				vm.current.weather = vm.current.conditions.data.current_observation.weather;
-				vm.current.icon = vm.current.conditions.data.current_observation.icon_url;
+		vm.current = {};
+
+		forecastSvc.getCurrentConditions().viaUserInput(vm.manual.userInput).then(
+			/* request for weather data was successful, so:
+				- set current
+				- reset manual */
+			function(result) {
+				vm.current = result;
 				vm.manual.userInput = "";
-				vm.manual.apiOutput = "";
+				vm.manual.ongoingQuery = false;
+			},
+			/* request for weather data wasn't successful, so:
+				- set manual.notFound
+				- reset current, manual.userInput, manual.ongoingQuery */
+			function(error) {
+				console.log("error");
+				vm.manual.userInput = "";
+				vm.manual.ongoingQuery = false;
+				vm.manual.notFound = true;
+				vm.current = {};
 			}
-			else {
-				// location entered matches several locations
-				if (result.data.response.results) {
-					vm.manual.apiOutput = "zmw:" + result.data.response.results[0].zmw;
-					// recursive call of manualQuery, this time using apiOutput data
-					vm.manualQuery();
-				}
-				// no location matches location entered
-				else {
-					vm.manual.userInput = "";
-					vm.manual.apiOutput = "";
-					vm.manual.ongoingQuery = false;
-					vm.manual.notFound = true;
-				}
-			}
-		})			
-	}
+		);			
+	};
 	
 	vm.automaticQuery();
 	
--- Frontend-Local-Weather/js/services.js
+++ Frontend-Local-Weather/js/services.js
@@ -86,37 +86,111 @@ angular.module("localWeather").service("forecastSvc", ["$q", "$http", function($
 
 	var self = this;
 	var key = ""; // enter your API key here
+	
+	
+	  /************************************************
+	    getCurrentConditions function
+		-------------------------------------------------
+		- returns conditions data from wunderground api
+		- can handle different types of requests:
+		--- coordinates
+		--- autoip
+		--- user text input
+		- internal executeQuery function fetches data
+			from api; it operates recursively if request
+			ended up in a not unique location
+		- if successful, a proper response object is
+			built in internal buildResponse function
+		*************************************************/
+	self.getCurrentConditions = function() {
 
-	self.getCurrentConditions = function(input) {
-		// query via coordinates
-		if (input.coords) {
+		/* INTERNAL FUNCTIONALITY */
+		
+		/* buildResponse function
+		   - expects a proper conditions object from weather underground api, ie.
+			 	 data.current_observation is already stripped away
+			 - returns object with current conditions */
+		function buildResponse(input) {
+
+			var output = {};
+
+			// store all information in conditions
+			output.conditions = input;
+			
+			// shortcuts for relevant pieces of information
+			output.place = output.conditions.display_location.full;
+			output.temperatureC = output.conditions.temp_c;
+			output.temperatureF = output.conditions.temp_f;
+			output.description = output.conditions.weather;
+			output.icon = output.conditions.icon_url;
+
+			return output;
+		};
+		
+		/* executeQuery function
+			- executes query to weather underground api
+			- handles various responses:
+			--- successful request returning proper weather data for location entered
+			--- initializes another request if location entered did not result in
+					unique location;
+					Please note:
+					the first location in data.response.results is used, so we leave it
+					to users to change location manually if that wasn't the one they were looking for
+			--- rejects if location entered did not match any location within weather
+					underground api */
+		function executeQuery(query) {
+			
 			return $q(function(resolve, reject) {
-				$http.get("http://api.wunderground.com/api/" + key + "/conditions/lang:EN/q/" + input.coords.latitude + "," + input.coords.longitude + ".json").then(
+				
+				$http.get("http://api.wunderground.com/api/" + key + "/conditions/lang:EN/q/" + query + ".json").then(
+					
+					// api request worked without error
 					function(result) {
-						// TODO: test for result.data.current_observation could be put here instead of putting it into controller
-						resolve(result);
+						// query was successful and returned weather data
+						if (result.data.current_observation) {
+							resolve(buildResponse(result.data.current_observation));
+						}
+						// query did not return weather data
+						else {
+							// query matched several locations, so use first zmw code and call executeQuery recursively
+							if (result.data.response.results) {
+								resolve(executeQuery("zmw:" + result.data.response.results[0].zmw));
+							}
+							// query matched no location at all
+							else {
+								reject();
+							}
+						}
 					},
+					
+					// error happened during get request
 					function(error) {
 						reject(error);
 					}
 				);
 			});
-		}
-		// query via manual input, zwm-id or autoip feature
-		else {
-			return $q(function(resolve, reject) {
-				$http.get("http://api.wunderground.com/api/" + key + "/conditions/lang:EN/q/" + input + ".json").then(
-					// TODO: test for result.data.current_observation could be put here instead of putting it into controller
-					function(result) {
-						resolve(result);
-					},
-					function(error) {
-						reject(error);
-					}
-				);
-			});
+		};
+		
+		/* EXTERNALLY ACCESSIBLE FUNCTIONALITY */
+		
+		return {
+
+			// query via coordinates
+			viaCoords: function(input) {
+				return (executeQuery(input.coords.latitude + "," + input.coords.longitude));
+			},
+
+			// query using weather undergrounds autoip feature
+			viaAutoIp: function() {
+				return (executeQuery("autoip"));
+			},
+			
+			// query via manual input, zwm-id or autoip feature
+			viaUserInput: function(input) {
+				return (executeQuery(input));
+			}
 			
 		}
-	};
+	}; // /self.getCurrentConditions
 	
 }]);
